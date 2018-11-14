2600acac5ba2a8ad03f3cddf51f3dcb56951319c
Paul Tobin <tobster111@gmail.com>
2018-11-14 11:48:43 +0000

    Added the launches component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import logo from './logo.png';
 import './App.css';
+
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} alt="SpaceX" style={{width: 300, display: 'block', margin: 'auto' }} />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: 'block', margin: 'auto' }}
+          />
+          <Launches />
+        </div>
+      </ApolloProvider>
     );
   }
 }
