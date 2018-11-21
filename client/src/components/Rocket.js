4ed59ef382b171dd5c927bd0c061b0d4369a283c
Paul Tobin <tobster111@gmail.com>
2018-11-21 19:24:54 +0000

    Further styling to LaunchItem

--- client/src/components/Rocket.js
+++ client/src/components/Rocket.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Rocket extends Component {
   render() {
     return (
       <div>
         <h1>Rocket</h1>
+        <Link to="/" className="btn btn-secondary">
+          Back
+        </Link>
       </div>
     );
   }
